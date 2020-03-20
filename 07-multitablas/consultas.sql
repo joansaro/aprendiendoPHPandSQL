421dabe6b53492b92e0f4b3c9b1e4536d1fa9f4f
Andrés <joansaro.sant@gmail.com>
2020-03-20 10:50:40 -0600

    Clase-05 (Actualizaión)
    Clase 05 Actualizacion de consultas multitablas con INNER JOIN, LESFT JOIN y RIGHT JOIN

--- 07-multitablas/consultas.sql
+++ 07-multitablas/consultas.sql
@@ -8,9 +8,30 @@ SELECT e.id, e.titulo, u.nombre AS 'AUTOR', c.nombre AS 'CATEGORIA'
 FROM entradas e, usuarios u, categorias c
 WHERE e.usuario_id = u.id AND e.categoria_id = c.id;
 
+# INNER JOIN#
+SELECT e.id, e.titulo, u.nombre AS 'AUTOR', c.nombre AS 'CATEGORIA'
+FROM entradas e
+INNER JOIN usuarios u ON e.usuario_id = u.id
+INNER JOIN categorias c ON e.categoria_id = c.id;
+
 # MOSTRAR EL NOMBRE DE LAS CATEGORIAS Y AL LADO CUANTAS ENTRADAS TIENEN #
 SELECT c.nombre, COUNT(e.id) FROM categorias c, entradas e
 WHERE e.categoria_id = c.id GROUP BY e.categoria_id;
+
+# INNER JOIN #
+SELECT c.nombre, COUNT(e.id) FROM categorias c
+INNER JOIN entradas e ON e.categoria_id =c.id
+ORDER BY e.categoria_id;
+
+# LEFT JOIN #
+SELECT c.nombre, COUNT(e.id) FROM categorias c
+LEFT JOIN entradas e ON e.categoria_id = c.id
+ORDER BY e.categoria_id;
+
+# RIGHT JOIN #
+SELECT c.nombre, COUNT(e.id) FROM entradas e
+RIGHT JOIN categorias c ON e.categoria_id = c.id
+ORDER BY e.categoria_id;
 
 # MOSTRAR EL EMAIL DE LOS USUARIOS Y AL LADO CUANTAS ENTRADAS TIENEN #
 SELECT u.email, COUNT(e.id)
