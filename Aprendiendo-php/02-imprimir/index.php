0d917a9564909d34579f61c9ae7dbc147ee0a278
Andrés <joansaro.sant@gmail.com>
2020-03-23 18:42:43 -0600

    Reorganización del repocitorio
    Reorganizando el repocitorio de aprendiendoPHPandSQL

---
+++ Aprendiendo-php/02-imprimir/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Imprimir por pantalla - Master en PHP</title>
+</head>
+<body>
+    <h1>Master en PHP - Joansaro</h1>
+    <?='Bienvenido a PHP'?>
+    <?php
+    //Comentario
+    echo '<h3>Listado de Videojuegos</h3>';
+    
+    /*
+    Esta es una lista 
+    de Videojuegos
+    modernos
+    */
+    echo '<ul>'
+        .'<li>GTA</li>'
+        .'<li>FIFA</li>'
+        .'<li>TLOZ</li>'
+        .'</ul>';
+
+    //echo 'Hola mundo';    
+    //Parrafo explicativo
+    echo '<p>Esta es toda' . '-' . ' lidta de juegos </p>';
+    ?>
+
+</body>
+</html>
+
+
+
