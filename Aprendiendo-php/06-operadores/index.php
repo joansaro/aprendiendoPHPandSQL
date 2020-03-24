8b06dd067196ad795c8b1349e75245c3ffe1aca5
Andrés <joansaro.sant@gmail.com>
2020-03-24 17:54:59 -0600

    Clase-02
    Actualización de la clase 02 de php

---
+++ Aprendiendo-php/06-operadores/index.php
@@ -0,0 +1,15 @@
+<?php
+
+//Operadores de asignacion
+$edad = 23;
+
+echo $edad. '<br>';
+
+//$edad = $edad + 5;
+echo ($edad += 5);
+
+echo ($edad -= 5);
+
+echo ($edad *= 5);
+
+echo ($edad /= 5);
